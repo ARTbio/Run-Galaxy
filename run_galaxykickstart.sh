a9692e76730f35ce123662a7f72f437833f5f60e
Christophe Antoniewski <drosofff@gmail.com>
2017-11-20 18:35:20 +0100

    add script to run galaxy-kickstart in native Ubuntu-trusty VM

---
+++ run_galaxykickstart.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+
+apt-get install -y python-pip python-dev git htop
+pip install -U pip
+pip install ansible==2.2
+ansible --version
+git clone https://github.com/ARTbio/GalaxyKickStart.git
+cd GalaxyKickStart/
+ansible-galaxy install -r requirements_roles.yml -p roles/ -f
+echo "\nEditing group_vars/all\n"
+sed -i -e 's/galaxy_manage_trackster: true/galaxy_manage_trackster: false/' group_vars/all
+ansible-playbook -i inventory_files/galaxy-kickstart galaxy.yml
+echo "\nSleeping 15 sec before restarting Galaxy server\n"
+echo "zzzz zzzz..."
+sleep 15
+supervisorctl restart galaxy:
+
