6f6384e9afdbaf22913e174691fba96f41b9dd20
Christophe Antoniewski <drosofff@gmail.com>
2017-11-21 12:25:23 +0100

    improve run_galaxykickstart.sh and make it executable

--- run_galaxykickstart.sh
+++ run_galaxykickstart.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -e
-apt-get update -y
 apt-get install -y python-pip python-dev python-setuptools git htop
+echo "Upgrading pip to v 1.9\n"
 pip install -U pip
-pip install ansible==2.2
+pip --version
+/usr/local/bin/pip install ansible==2.2
 ansible --version
 git clone https://github.com/ARTbio/GalaxyKickStart.git
 cd GalaxyKickStart/
@@ -14,4 +15,6 @@ ansible-playbook -i inventory_files/galaxy-kickstart galaxy.yml
 echo "\nSleeping 15 sec before restarting Galaxy server\n"
 echo "zzzz zzzz..."
 sleep 15
-supervisorctl restart galaxy:
+supervisorct stop galaxy:
+killall uwsgi
+supervisorctl start galaxy:
