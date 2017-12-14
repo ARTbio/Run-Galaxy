e939153d9e9c1d1cb9a9b53a8525f8722dffd8af
Christophe Antoniewski <drosofff@gmail.com>
2017-12-14 18:21:00 +0100

    freeze the run_galaxykickstart.sh script. Issue with the error termination in supervisorctl will have been fixed next year

--- deployment_scripts/run_galaxykickstart.sh
+++ deployment_scripts/run_galaxykickstart.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 apt-get install -y python-pip python-dev python-setuptools git htop
-echo "Upgrading pip to v 1.9\n"
+echo "Upgrading pip to v 1.9"
 pip install -U pip
 pip --version
 /usr/local/bin/pip install ansible==2.2
@@ -9,10 +9,11 @@ ansible --version
 git clone https://github.com/ARTbio/GalaxyKickStart.git
 cd GalaxyKickStart/
 ansible-galaxy install -r requirements_roles.yml -p roles/ -f
-echo "\nEditing group_vars/all\n"
+echo "Editing group_vars/all"
 sed -i -e 's/galaxy_manage_trackster: true/galaxy_manage_trackster: false/' group_vars/all
 ansible-playbook -i inventory_files/galaxy-kickstart galaxy.yml
-echo "\nSleeping 30 sec before restarting Galaxy server\n"
+echo "Sleeping 15 sec before restarting Galaxy server"
 echo "zzzz zzzz..."
-sleep 60
+sleep 15
 supervisorctl restart galaxy:
+echo "if galaxy:galaxy_web failed to restart with error termination, just do `killall uwsgi; supervisorctl restart galaxy:`"
