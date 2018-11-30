06e60aabb45a22fd63b251effba3b276d6212538
Christophe Antoniewski <drosofff@gmail.com>
2018-11-30 12:47:24 +0100

    simplify ansible deployment script and adapt doc

--- deployment_scripts/run_galaxykickstart.sh
+++ deployment_scripts/run_galaxykickstart.sh
@@ -4,16 +4,13 @@ apt-get install -y python-pip python-dev python-setuptools git htop
 echo "Upgrading pip to v 1.9"
 pip install -U pip
 pip --version
-/usr/local/bin/pip install ansible==2.2
+/usr/local/bin/pip install ansible==2.4
 ansible --version
-git clone https://github.com/ARTbio/GalaxyKickStart.git
+git clone https://github.com/ARTbio/GalaxyKickStart.git -b bio-genomes-2018
 cd GalaxyKickStart/
 ansible-galaxy install -r requirements_roles.yml -p roles/ -f
-echo "Editing group_vars/all"
-sed -i -e 's/galaxy_manage_trackster: true/galaxy_manage_trackster: false/' group_vars/all
 ansible-playbook -i inventory_files/galaxy-kickstart galaxy.yml
 echo "Sleeping 15 sec before restarting Galaxy server"
 echo "zzzz zzzz..."
 sleep 15
 supervisorctl restart galaxy:
-echo "if galaxy:galaxy_web failed to restart with error termination, just do `killall uwsgi; supervisorctl restart galaxy:`"
