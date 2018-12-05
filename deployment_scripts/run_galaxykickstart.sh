c0a093dd6c8398794ea8141e0775b69cd31e7a0a
drosofff <drosofff@gmail.com>
2018-12-05 02:29:55 +0100

    install pip==18.01

--- deployment_scripts/run_galaxykickstart.sh
+++ deployment_scripts/run_galaxykickstart.sh
@@ -3,7 +3,7 @@ set -e
 apt update -y
 apt install -y python-pip python-dev python-setuptools git htop
 echo "Upgrading pip"
-pip install -U pip
+pip install pip==18.1
 pip --version
 pip install ansible==2.4
 ansible --version
