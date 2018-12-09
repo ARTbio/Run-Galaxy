ffeb65aca97f588e6e9ab06b7d4d963db003081e
drosofff <drosofff@gmail.com>
2018-12-09 12:54:25 +0100

    parametize run_galaxykickstart.sh to take biogen2018 or pasteur-2018 branches

--- deployment_scripts/run_galaxykickstart.sh
+++ deployment_scripts/run_galaxykickstart.sh
@@ -7,11 +7,8 @@ pip install -U pip
 pip --version
 pip install ansible==2.4
 ansible --version
-git clone https://github.com/ARTbio/GalaxyKickStart.git -b biogen2018
+git clone https://github.com/ARTbio/GalaxyKickStart.git -b $1
 cd GalaxyKickStart/
 ansible-galaxy install -r requirements_roles.yml -p roles/ -f
 ansible-playbook -i inventory_files/galaxy-kickstart galaxy.yml
-echo "Sleeping 15 sec before restarting Galaxy server"
-echo "zzzz zzzz..."
-sleep 15
-supervisorctl restart galaxy:
+echo "end of deployment\n"
