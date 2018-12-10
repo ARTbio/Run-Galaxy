b6ec88bd38545c21c0eaf63aaff6c99e530a5ab9
Christophe Antoniewski <drosofff@gmail.com>
2018-12-10 10:36:06 +0100

    add install pip cryptography==2.2.2 to the GCE script run_galaxykickstart.sh

--- deployment_scripts/run_galaxykickstart.sh
+++ deployment_scripts/run_galaxykickstart.sh
@@ -5,6 +5,7 @@ apt install -y python-pip python-dev python-setuptools git htop
 echo "Upgrading pip"
 pip install -U pip
 pip --version
+pip install cryptography==2.2.2
 pip install ansible==2.4
 ansible --version
 git clone https://github.com/ARTbio/GalaxyKickStart.git -b $1
