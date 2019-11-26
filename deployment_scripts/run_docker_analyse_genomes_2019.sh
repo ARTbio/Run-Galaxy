d3476103f01c9cd1d7a9632acd81c97cd54f264d
Christophe Antoniewski <drosofff@users.noreply.github.com>
2019-11-26 22:04:33 +0100

    Update run_docker_analyse_genomes_2019.sh

--- deployment_scripts/run_docker_analyse_genomes_2019.sh
+++ deployment_scripts/run_docker_analyse_genomes_2019.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
-# run `sh run_docker_analyse_genomes_2019 artbio/analyse_genomes:2019`
+# run `sh run_docker_analyse_genomes_2019`
 set -e
 echo "Now pulling the artbio/analyse_genomes:2019 docker image from DockerHub\n"
 # supervisorctl stop all
-docker pull $1
+docker pull artbio/analyse_genomes:2019
 echo "Running artbio/analyse_genomes:2019 docker container\n"
 mkdir /galaxy_export /galaxy_tmp && chown 1450:1450 /galaxy_export /galaxy_tmp
 export DOCKER_INSTANCE=`docker run -d -p 80:80 -p 21:21 -p 8800:8800 \
@@ -14,9 +14,7 @@ export DOCKER_INSTANCE=`docker run -d -p 80:80 -p 21:21 -p 8800:8800 \
           -e GALAXY_CONFIG_ENABLE_BETA_WORKFLOW_MODULES=True \
           -v /galaxy_tmp:/tmp \
           -v /galaxy_export:/export \
-          $1`
-echo "The $1 docker container is deploying...\n"
-sleep 120
-echo "The $1 docker container is up and running\n"
-docker logs  $DOCKER_INSTANCE
-docker exec $DOCKER_INSTANCE sudo supervisorctl restart galaxy:
+          analyse_genomes:2019`
+echo "The analyse_genomes:2019 docker container is deploying...\n"
+echo "Press Ctrl-C to interrupt this log and start using the container...\n"
+docker logs -f  $DOCKER_INSTANCE
