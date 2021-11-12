96c575f82029d34f3acabea14feb9d3172d86adb
drosofff <drosofff@gmail.com>
2021-11-12 19:26:37 +0100

    Update run_bare_galaxy_2021.sh

--- deployment_scripts/run_bare_galaxy_2021.sh
+++ deployment_scripts/run_bare_galaxy_2021.sh
@@ -4,7 +4,7 @@ git clone https://github.com/galaxyproject/galaxy.git -b release_21.05
 cd ~/galaxy/
 cp config/galaxy.yml.sample config/galaxy.yml
 sed -i "s/http: 127.0.0.1:8080/http: 0.0.0.0:80/" config/galaxy.yml
-sed -i "s/#admin_users: ''/admin_users: 'admin@galaxy.org'/" config/galaxy.yml
+sed -i "s/#admin_users: null/admin_users: 'admin@galaxy.org'/" config/galaxy.yml
 rm -rf ~/galaxy/client ~/galaxy/static
 cd ~/galaxy && wget https://mydeepseqbucket.s3.amazonaws.com/bare.client.tar.gz \
     https://mydeepseqbucket.s3.amazonaws.com/bare.static.tar.gz
