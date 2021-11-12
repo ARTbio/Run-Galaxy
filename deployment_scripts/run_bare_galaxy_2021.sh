a80693881eb85d14027a5ea2036c90e04f01eecd
drosofff <drosofff@gmail.com>
2021-11-12 18:22:44 +0100

    adapt deployment scripts

--- deployment_scripts/run_bare_galaxy_2021.sh
+++ deployment_scripts/run_bare_galaxy_2021.sh
@@ -6,12 +6,12 @@ cp config/galaxy.yml.sample config/galaxy.yml
 sed -i "s/http: 127.0.0.1:8080/http: 0.0.0.0:80/" config/galaxy.yml
 sed -i "s/#admin_users: ''/admin_users: 'admin@galaxy.org'/" config/galaxy.yml
 rm -rf ~/galaxy/client ~/galaxy/static
-cd ~/galaxy && wget https://mydeepseqbucket.s3.amazonaws.com/client.tar.gz \
-    https://mydeepseqbucket.s3.amazonaws.com/static.tar.gz
-cd ~/galaxy && tar -xvf static.tar.gz && tar -xvf client.tar.gz
+cd ~/galaxy && wget https://mydeepseqbucket.s3.amazonaws.com/bare.client.tar.gz \
+    https://mydeepseqbucket.s3.amazonaws.com/bare.static.tar.gz
+cd ~/galaxy && tar -xzf bare.static.tar.gz && tar -xzf bare.client.tar.gz
 if [[ -d "/mnt/mydatalocal" ]]
     then
     mv ~/galaxy /mnt/mydatalocal
     cd /mnt/mydatalocal
 fi
-time sh run.sh
+sh run.sh
