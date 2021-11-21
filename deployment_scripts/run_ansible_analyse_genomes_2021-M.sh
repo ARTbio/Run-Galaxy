d3bed01fff8b24eda073cf3f4df8eaa0279a67f1
drosofff <drosofff@gmail.com>
2021-11-22 00:27:35 +0100

    adjusting deployment scripts

--- deployment_scripts/run_ansible_analyse_genomes_2021-M.sh
+++ deployment_scripts/run_ansible_analyse_genomes_2021-M.sh
@@ -9,11 +9,11 @@ ansible-galaxy install -r requirements_roles.yml -p roles/ -f
 ansible-playbook -i inventory_files/Analyse_genomes galaxy.yml
 cd /home/galaxy/tool_dependencies
 rm -rf _conda
-wget http://usegalaxy.sorbonne-universite.fr/nextcloud/index.php/s/s6AAtDwR5at46bb/download/_conda.pigz.tar.gz
+wget https://usegalaxy.sorbonne-universite.fr/nextcloud/index.php/s/w2c6GjXCfwPkwiW/download/_conda.pigz.tar.gz
 apt install -y pigz
+echo "Uncompressing conda archive\n"
 tar -I pigz -xf _conda.pigz.tar.gz
 chown -R galaxy:galaxy _conda
-rm -rf _conda_tar.gz
 cd ~/galaxykickstart
 ansible-playbook -i inventory_files/Analyse_genomes galaxy_tool_install.yml
 echo "end of deployment\n"
