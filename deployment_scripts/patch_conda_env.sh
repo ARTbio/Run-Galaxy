75e5429265bdc52ae5c36f87b2a0aa1e8ebd717e
drosofff <drosofff@gmail.com>
2021-11-23 19:46:05 +0100

    Create patch_conda_env.sh

---
+++ deployment_scripts/patch_conda_env.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+apt update -y
+apt install -y pigz
+echo "deleting conda env\n"
+rm -rf /home/galaxy/tool_dependencies/_conda*
+echo "Dowloading _conda.pigz.tar.gz from Amazon S3\n"
+wget https://mydeepseqbucket.s3.amazonaws.com/_conda.pigz.tar.gz
+echo "extracting conda archive... zzzz\n"
+tar -I pigz -xf _conda.pigz.tar.gz
+echo "Ensure _conda is owned by galaxy user\n"
+chown -R galaxy:galaxy _conda
+echo "Leaving _conda.pigz.tar.gz in /home/galaxy/tool_dependencies/\n"
+echo "restarting Galaxy server\n"
+supervisorctl restart galaxy:
